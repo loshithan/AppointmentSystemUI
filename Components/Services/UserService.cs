da7cca9b08c278fcb2beb4b846807b6d28a4a392
loshithan invorg <lparamaguru@invorg.com>
2025-03-20 15:29:40 +0530

    authorization multi role nav & other places implemented

--- Components/Services/UserService.cs
+++ Components/Services/UserService.cs
@@ -26,6 +26,20 @@ namespace AppointmentUI.Components.Services
             return null; // User is not authenticated
         }
 
+        public async Task<string?> GetUserNameAsync()
+        {
+            var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (user.Identity?.IsAuthenticated == true)
+            {
+                // Replace "nameid" with the actual claim type for UserId in your token
+                return user.Claims.FirstOrDefault(c => c.Type == "unique_name")?.Value;
+            }
+
+            return null; // User is not authenticated
+        }
+
         public async Task<string?> GetClaimValueAsync(string claimType)
         {
             var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
